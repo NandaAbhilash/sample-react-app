b3b04c0a8ae77f65dd5cb50f30c144f27506f2c1
NandaAbhilash <nandaabhilash9119@gmail.com>
2026-03-04 09:52:51 +0000

    Update API actions

--- src/api/actions.ts
+++ src/api/actions.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosError } from "axios";
 
-const API_URL = "https://studious-train-7pq794x5gr72xrrv-3000.app.github.dev/api";
+const API_URL = "https://cautious-space-winner-g479w7v4rgqr39x55-3000.app.github.dev/api";
 
 export const getWeatherData = async (city: string): Promise<WeatherData> => {
   return new Promise<WeatherData>((resolve, reject) => {
